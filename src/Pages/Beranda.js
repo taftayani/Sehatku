128e94c83eaa546811e67d5f887ee5417c707482
taftayani <taftayani123@gmail.com>
2019-03-22 14:59:01 +0700

    #upload file

---
+++ src/Pages/Beranda.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Beranda extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>TESSSSS</h1>
+            </div>
+        )
+    }
+}
+export default Beranda;
