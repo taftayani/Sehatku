1f34c7c00d041037fd419a72f245f4e7655dfef0
taftayani <taftayani123@gmail.com>
2019-03-30 22:27:02 +0700

    #added file and folder

---
+++ src/Component/Homepage/FirstSection.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default class FirstSection extends React.Component{
+    render(){
+        return(
+            <div>
+                <div className="container-fluid first-bg">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-xl-8">
+                                <div className="row">
+                                    <div className="col-12">
+                                        <h1 className="heading-first">Sehatku Adalah Sebuah Aplikasi Kesehatan Sebagai Alat
+                                            Konsultasi dengan Dokter Yang anda Inginkan
+                                        </h1>
+                                    </div>
+                                    <div className="col-12 ">
+                                        <div className="img-heading-first">
+                                        
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="col-xl-4">
+                                <div className="img-heading-second">
+                                
+                                </div>
+                            </div>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+        )
+    }
+}
--- src/Pages/Beranda.js
+++ src/Pages/Beranda.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
+import Head from '../Input/Header'
+import Header from '../Component/Homepage/HeaderFirst'
+import First from '../Component/Homepage/FirstSection'
 class Beranda extends React.Component{
     render(){
         return(
             <div>
-                <h1>TESSSSS</h1>
+                <Head/>
+                <div className="container-fluid navbar-bg">
+                    <div className="container">
+                        <Header/>
+                    </div>
+                </div>
+                <First/>
+                <style jsx>{
+                   `
+                    .navbar-bg{
+                        position:fixed;
+                        background: white;
+                        box-shadow:0px 3px 6px 2px grey;
+                    }
+                    .first-bg{
+                        background: #E8E8E8;
+                        padding-top:90px;
+                        padding-bottom:70px;
+                    }
+                    .heading-first{
+                        font-family:Neo Sans W1G;
+                        color:#535D65;
+                    }
+                    .img-heading-first{
+                        background:#A0A4A8;
+                        height:90px;
+                        border-radius:8px;
+                        width:70%;
+                        margin-top:30px;
+                    }
+                    .img-heading-second{
+                        background:#A0A4A8;
+                        height:100%;
+                        border-radius:8px;
+                        width:80%;
+                    }
+                   `
+                }
+            </style>
             </div>
+           
         )
     }
 }
